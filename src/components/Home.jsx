c03458d382ee3f523930cfa95ac07c1634d4aecc
rxgacheru <roygacheru01@gmail.com>
2024-04-29 14:54:34 +0300

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const   Home = () => {
       < a href='/contact'>
       <button className="text-white bg-yellow-900 hover:bg-yellow-950 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-large rounded-full text-sm px-9 py-5 text-center me-2 mb-2 dark:focus:ring-yellow-900 text-2xl font-bold ">Contact Us</button></a></div>
       <div className='w-1/2 ml-12 mt-4 mb-5 h-100 w-5000'>
-      <img src='src\components\background.avif' className='h-auto max-w-lg' ></img></div>
+      <img src='https://i0.wp.com/tryfreelance.co/wp-content/uploads/2024/04/1712312239587-scaled.jpg?resize=768%2C1024&ssl=1' className='h-auto max-w-lg' ></img></div>
     </div>
   )
   }
